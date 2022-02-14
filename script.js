1a89b8a272b0c4c78408d1f0e81c145ae5867022
Azaria Fowler <fowler.azaria@gmail.com>
2022-02-13 21:03:55 -0700

    loaded mediapipe library and holisitic model

---
+++ script.js
@@ -0,0 +1,76 @@
+const videoElement = document.getElementsByClassName('input_video')[0];
+const canvasElement = document.getElementsByClassName('output_canvas')[0];
+const canvasCtx = canvasElement.getContext('2d');
+
+const mpHolistic = window;
+const drawingUtils = window;
+
+function onResults(results) {
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+
+    if (results.rightHandLandmarks) {
+        console.log(results.rightHandLandmarks);
+
+        drawingUtils.drawConnectors(
+            canvasCtx, results.rightHandLandmarks, mpHolistic.HAND_CONNECTIONS,
+            { color: 'white' });
+
+        drawingUtils.drawLandmarks(canvasCtx, results.rightHandLandmarks, {
+            color: 'white',
+            fillColor: 'rgb(0,217,231)',
+            lineWidth: 2,
+            radius: (data) => {
+                return drawingUtils.lerp(data.from.z, -0.15, .1, 10, 1);
+            }
+        });
+    }
+
+    if (results.leftHandLandmarks) {
+        console.log(results.leftHandLandmarks);
+
+        drawingUtils.drawConnectors(
+            canvasCtx, results.leftHandLandmarks, mpHolistic.HAND_CONNECTIONS,
+            { color: 'white' });
+
+        drawingUtils.drawLandmarks(canvasCtx, results.leftHandLandmarks, {
+            color: 'white',
+            fillColor: 'rgb(0,217,231)',
+            lineWidth: 2,
+            radius: (data) => {
+                return drawingUtils.lerp(data.from.z, -0.15, .1, 10, 1);
+            }
+        });
+    }
+
+    canvasCtx.restore();
+}
+
+const holistic = new Holistic({
+    locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`;
+    }
+});
+
+holistic.setOptions({
+    selfieMode: true,
+    modelComplexity: 1,
+    smoothLandmarks: true,
+    enableSegmentation: false,
+    smoothSegmentation: true,
+    minDetectionConfidence: 0.5,
+    minTrackingConfidence: 0.8,
+    effect: 'background',
+});
+
+holistic.onResults(onResults);
+
+const camera = new Camera(videoElement, {
+    onFrame: async () => {
+        await holistic.send({ image: videoElement });
+    },
+    width: 1280,
+    height: 720,
+});
+videoElement.classList.add('selfie');
+camera.start();
