5612b5c8ceace8c94ec44495514b2aadc83ee83e
Azaria Fowler <fowler.azaria@gmail.com>
2022-03-21 17:13:49 -0700

    Took out the dynamic CDN loading; need a better way!

--- public/src/js/gesture.js
+++ public/src/js/gesture.js
@@ -5,8 +5,6 @@ class DOMElement {
         this.id = uuid;
         this.selected = false;
         this.hovered = false;
-        this.landmarkRadius = 0.0;
-        this.continueGrowing = true;
         this.userWantsToPickUpElement = false;
         this.userPickedUpElement = false;
     }
@@ -27,7 +25,9 @@ export class Gesture {
     cursor; // Reference for the user's cursor.
     mediapipe; // Reference to MediaPipe engine.
 
-    constructor() { // Default constructor.
+    constructor(applicationElementID) { // Default constructor.
+        // Step 0: Add the nessessary CDNs. Better way to bundle?
+
         // Step 1: Assign UUIDs to all the elements on the DOM for easy access.
         var allElements = document.getElementsByTagName("*"); // Get all the elements on the DOM.
         for (var i = 0, max = allElements.length; i < max; i++) {
@@ -37,6 +37,29 @@ export class Gesture {
 
         // Step 2: Create a cursor reference.
         this.cursor = new Cursor();
+
+        // Step 3: Restructure the DOM for MediaPipe.
+        let applicationElement = document.getElementById(applicationElementID);
+
+        let container = "<div id='container'></div>";
+        let inputVideo = "<video id='input-video'></video>";
+        let canvasContainer = "<div id='canvas-container'></div>";
+        let canvas = "<canvas id='output-canvas' width='1280px' height='720px'></canvas>"
+
+        document.body.innerHTML = container;
+        document.getElementById("container").innerHTML = inputVideo + canvasContainer;
+        document.getElementById("canvas-container").innerHTML = canvas;
+        document.getElementById("container").appendChild(applicationElement);
+
+        // Step 4: Turn on MediaPipe.
+        let videoElement = document.getElementById("input-video");
+        let canvasElement = document.getElementById("output-canvas");
+
+        // onResults() is a function that returns the Holistic landmarks.
+        this.mediapipe = new MediaPipe(videoElement, canvasElement);
+        this.mediapipe.holisticSettings.onResults(async (results) => {
+            this.mediapipe.onResults(results);
+        });
     }
 
     // ---------------------------------------- Helper Methods ----------------------------------------
@@ -49,244 +72,56 @@ export class Gesture {
 
     // ---------------------------------------- Client Methods ----------------------------------------
 
-    // Create a default MediaPipe enviroment.
-    async gesturize(applicationElementID) {
-        // Step 0: Add the nessessary CDNs. Better way to bundle?
-
-        let head = document.getElementsByTagName('head')[0];
-        let nessessaryCDNS = [
-            "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js",
-            "https://cdn.jsdelivr.net/npm/@mediapipe/camera_utils/camera_utils.js",
-            "https://cdn.jsdelivr.net/npm/@mediapipe/control_utils/control_utils.js",
-            "https://cdn.jsdelivr.net/npm/@mediapipe/drawing_utils/drawing_utils.js",
-            "https://cdn.jsdelivr.net/npm/@mediapipe/holistic/holistic.js",
-            "https://unpkg.com/@tensorflow/tfjs-core@3.7.0/dist/tf-core.js",
-            "https://unpkg.com/@tensorflow/tfjs-converter@3.7.0/dist/tf-converter.js",
-            "https://unpkg.com/@tensorflow/tfjs-backend-webgl@3.7.0/dist/tf-backend-webgl.js",
-            "https://unpkg.com/@tensorflow-models/handpose@0.0.7/dist/handpose.js",
-            "https://cdn.jsdelivr.net/npm/fingerpose@0.1.0/dist/fingerpose.min.js"
-        ];
-        let loadedCDNS = 0;
-
-        var script;
-        for (var i = 0; i < nessessaryCDNS.length; i++) {
-            script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.src = nessessaryCDNS[i];
-            script.crossOrigin = "anonymous";
-
-            script.onload = function () {
-                loadedCDNS += 1;
-            };
-
-            head.appendChild(script);
-        }
-
-        let checkCDNs = setInterval(function () {
-            if (loadedCDNS == nessessaryCDNS.length) {
-                console.log("CDNs have loaded...");
-                clearInterval(checkCDNs);
-                // Step 1: Restructure the DOM for MediaPipe.
-                let applicationElement = document.getElementById(applicationElementID);
-
-                let container = "<div id='container'></div>";
-                let inputVideo = "<video id='input-video'></video>";
-                let canvasContainer = "<div id='canvas-container'></div>";
-                let canvas = "<canvas id='output-canvas' width='1280px' height='720px'></canvas>"
-
-                document.body.innerHTML = container;
-                document.getElementById("container").innerHTML = inputVideo + canvasContainer;
-                document.getElementById("canvas-container").innerHTML = canvas;
-                document.getElementById("container").appendChild(applicationElement);
-
-                // Step 2: Turn on MediaPipe.
-                let videoElement = document.getElementById("input-video");
-                let canvasElement = document.getElementById("output-canvas");
-
-                // onResults() is a function that returns the Holistic landmarks.
-                this.mediapipe = new MediaPipe(videoElement, canvasElement);
-                this.mediapipe.holisticSettings.onResults(async (results) => {
-                    this.mediapipe.onResults(results);
-                });
-            } else {
-                console.log("CDNs still loading...");
-            }
-        }, 1000);
-    }
-
     // Body Part Detections ---------------------------------------------------------------------------
     onFace(callback) {
         console.log("Setting onFace()...");
-        if (this.mediapipe) {
-            this.mediapipe._faceLandmarksCallback = callback;
-        } else {
-            // Wait until MediaPipe has loaded.
-            let checkMediaPipe = setInterval(function () {
-                if (this.mediapipe) {
-                    console.log("MediaPipe has loaded...");
-                    clearInterval(checkMediaPipe);
-                    this.mediapipe._faceLandmarksCallback = callback;
-                } else {
-                    console.log("MediaPipe still loading...");
-                }
-            }, 1000);
-        }
+        this.mediapipe._faceLandmarksCallback = callback;
     }
 
     onPose(callback) {
         console.log("Setting onPose()...");
-        if (this.mediapipe) {
-            this.mediapipe._poseLandmarksCallback = callback;
-        } else {
-            // Wait until MediaPipe has loaded.
-            let checkMediaPipe = setInterval(function () {
-                if (this.mediapipe) {
-                    console.log("MediaPipe has loaded...");
-                    clearInterval(checkMediaPipe);
-                    this.mediapipe._poseLandmarksCallback = callback;
-                } else {
-                    console.log("MediaPipe still loading...");
-                }
-            }, 1000);
-        }
+        this.mediapipe._poseLandmarksCallback = callback;
     }
 
     onLeftHand(callback) {
         console.log("Setting onLeftHand()...");
-        if (this.mediapipe) {
-            this.mediapipe._leftHandLandmarksCallback = callback;
-        } else {
-            // Wait until MediaPipe has loaded.
-            let checkMediaPipe = setInterval(function () {
-                if (this.mediapipe) {
-                    console.log("MediaPipe has loaded...");
-                    clearInterval(checkMediaPipe);
-                    this.mediapipe._leftHandLandmarksCallback = callback;
-                } else {
-                    console.log("MediaPipe still loading...");
-                }
-            }, 1000);
-        }
+        this.mediapipe._leftHandLandmarksCallback = callback;
     }
 
     onRightHand(callback) {
-        console.log("Setting onRightHand()...");
-        if (this.mediapipe) {
-            this.mediapipe._rightHandLandmarksCallback = callback;
-        } else {
-            // Wait until MediaPipe has loaded.
-            let checkMediaPipe = setInterval(function () {
-                if (this.mediapipe) {
-                    console.log("MediaPipe has loaded...");
-                    clearInterval(checkMediaPipe);
-                    this.mediapipe._rightHandLandmarksCallback = callback;
-                } else {
-                    console.log("MediaPipe still loading...");
-                }
-            }, 1000);
-        }
+        this.mediapipe._rightHandLandmarksCallback = callback;
     }
 
     // Landmark Styles --------------------------------------------------------------------------
     styleFaceLandmarks(fillColor, borderColor, borderWidth, connectorColor, connectorWidth) {
-        console.log("Styling left hand landmarks...");
-        if (this.mediapipe) {
-            this.mediapipe.styleFaceLandmarks(fillColor, borderColor, borderWidth, connectorColor, connectorWidth);
-        } else {
-            // Wait until MediaPipe has loaded.
-            let checkMediaPipe = setInterval(function () {
-                if (this.mediapipe) {
-                    console.log("MediaPipe has loaded...");
-                    clearInterval(checkMediaPipe);
-                    this.mediapipe.styleFaceLandmarks(fillColor, borderColor, borderWidth, connectorColor, connectorWidth);
-                } else {
-                    console.log("MediaPipe still loading...");
-                }
-            }, 1000);
-        }
+        console.log("Styling face landmarks...");
+        this.mediapipe.styleFaceLandmarks(fillColor, borderColor, borderWidth, connectorColor, connectorWidth);
     }
 
     stylePoseLandmarks(fillColor, borderColor, borderWidth, connectorColor, connectorWidth) {
         console.log("Styling pose landmarks...");
-        if (this.mediapipe) {
-            this.mediapipe.stylePoseLandmarks(fillColor, borderColor, borderWidth, connectorColor, connectorWidth);
-        } else {
-            // Wait until MediaPipe has loaded.
-            let checkMediaPipe = setInterval(function () {
-                if (this.mediapipe) {
-                    console.log("MediaPipe has loaded...");
-                    clearInterval(checkMediaPipe);
-                    this.mediapipe.stylePoseLandmarks(fillColor, borderColor, borderWidth, connectorColor, connectorWidth);
-                } else {
-                    console.log("MediaPipe still loading...");
-                }
-            }, 1000);
-        }
+        this.mediapipe.stylePoseLandmarks(fillColor, borderColor, borderWidth, connectorColor, connectorWidth);
     }
 
     styleLeftHandLandmarks(fillColor, borderColor, borderWidth, connectorColor, connectorWidth) {
         console.log("Styling left hand landmarks...");
-        if (this.mediapipe) {
-            this.mediapipe.styleLeftHandLandmarks(fillColor, borderColor, borderWidth, connectorColor, connectorWidth);
-        } else {
-            // Wait until MediaPipe has loaded.
-            let checkMediaPipe = setInterval(function () {
-                if (this.mediapipe) {
-                    console.log("MediaPipe has loaded...");
-                    clearInterval(checkMediaPipe);
-                    this.mediapipe.styleLeftHandLandmarks(fillColor, borderColor, borderWidth, connectorColor, connectorWidth);
-                } else {
-                    console.log("MediaPipe still loading...");
-                }
-            }, 1000);
-        }
+        this.mediapipe.styleLeftHandLandmarks(fillColor, borderColor, borderWidth, connectorColor, connectorWidth);
     }
 
     styleRightHandLandmarks(fillColor, borderColor, borderWidth, connectorColor, connectorWidth) {
         console.log("Styling right hand landmarks...");
-        if (this.mediapipe) {
-            this.mediapipe.styleRightHandLandmarks(fillColor, borderColor, borderWidth, connectorColor, connectorWidth);
-        } else {
-            // Wait until MediaPipe has loaded.
-            let checkMediaPipe = setInterval(function () {
-                if (this.mediapipe) {
-                    console.log("MediaPipe has loaded...");
-                    clearInterval(checkMediaPipe);
-                    this.mediapipe.styleRightHandLandmarks(fillColor, borderColor, borderWidth, connectorColor, connectorWidth);
-                } else {
-                    console.log("MediaPipe still loading...");
-                }
-            }, 1000);
-        }
+        this.mediapipe.styleRightHandLandmarks(fillColor, borderColor, borderWidth, connectorColor, connectorWidth);
     }
 
     // Miscellaneous Settings -------------------------------------------------------------------
     showCamera() {
         console.log("Showing camera...");
-        if (document.getElementById("input-video")) {
-            let inputVideo = document.getElementById("input-video");
-            inputVideo.style.display = "block";
-            inputVideo.style.position = "fixed";
-            inputVideo.style.zIndex = -100;
-            inputVideo.style.height = "100%";
-            inputVideo.style.width = "100%";
-        } else {
-            // Wait until #input-video has loaded.
-            let checkCamera = setInterval(function () {
-                if (document.getElementById("input-video")) {
-                    console.log("#input-video has loaded...");
-                    clearInterval(checkCamera);
-                    let inputVideo = document.getElementById("input-video");
-                    inputVideo.style.display = "block";
-                    inputVideo.style.position = "fixed";
-                    inputVideo.style.zIndex = -100;
-                    inputVideo.style.height = "100%";
-                    inputVideo.style.width = "100%";
-                } else {
-                    console.log("#input-video still loading...");
-                }
-            }, 1000);
-        }
 
+        let inputVideo = document.getElementById("input-video");
+        inputVideo.style.display = "block";
+        inputVideo.style.position = "fixed";
+        inputVideo.style.zIndex = -100;
+        inputVideo.style.height = "100%";
+        inputVideo.style.width = "100%";
     }
 }
